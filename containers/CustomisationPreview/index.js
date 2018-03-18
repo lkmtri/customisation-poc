14695ac5a10b73bcd39d24b41e197b930e3cfa0b
Tri Le <tri.le@oddle.me>
2018-03-18 19:15:15 +0800

    add mobile desktop preview mode

--- containers/CustomisationPreview/index.js
+++ containers/CustomisationPreview/index.js
@@ -4,17 +4,52 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { initialState, reducers } from './reducers'
 import * as actions from './actions'
+import { STORE_KEY } from './constants'
 
-const STORE_KEY = '@@preview'
+const previewMode = {
+  mobile: 'MOBILE',
+  desktop: 'DESKTOP'
+}
 
 const Container = styled.div`
   background-color: #ddd;
   flex: 1;
+  display: flex;
+  flex-direction: column;
+`
+
+const PreviewActions = styled.div`
+  display: flex;
+  height: 50px;
+  min-height: 50px;
+  justify-content: space-between;
+  background-color: #888;
+`
+
+const PreviewActionGroup = styled.div`
+  display: flex;  
+  height: 100%;
+  align-items: center;
+`
+
+const PreviewActionItem = styled.div`
+  padding: 0 5px;
+  cursor: pointer;
+`
+
+const FrameContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const Frame = styled.iframe`
-  width: 100%;
-  height: 100%;
+  height: ${props => props.previewMode === previewMode.desktop ? '100%' : '600px'};
+  width: ${props => props.previewMode === previewMode.desktop ? '100%' : '400px'};
+  transition: height 200ms ease-in-out, width 200ms ease-in-out;
+  border: 1px #888 solid;
 `
 
 class CustomisationPreview extends React.PureComponent {
@@ -25,6 +60,10 @@ class CustomisationPreview extends React.PureComponent {
   static getInitialState = () => ({ [STORE_KEY]: initialState })
 
   static getReducers = () => ({ [STORE_KEY]: reducers })
+
+  state = {
+    mode: previewMode.desktop
+  }
 
   componentWillReceiveProps (nextProps) {
     if (nextProps.frameUrl !== this.props.frameUrl) {
@@ -40,6 +79,8 @@ class CustomisationPreview extends React.PureComponent {
     this.registerPreviewFrame()
   }
 
+  setPreviewMode = (mode) => () => this.setState({ mode })
+
   registerPreviewFrame = () => {
     if (window !== undefined) {
       const frame = document.getElementById('preview-frame')
@@ -51,10 +92,19 @@ class CustomisationPreview extends React.PureComponent {
 
   render () {
     const { frameUrl } = this.props
+    const { mode } = this.state
 
     return (
       <Container>
-        <Frame id='preview-frame' src={frameUrl} sandbox='allow-forms allow-scripts allow-same-origin allow-popups' />
+        <PreviewActions>
+          <PreviewActionGroup>
+            <PreviewActionItem onClick={this.setPreviewMode(previewMode.mobile)}>Mobile</PreviewActionItem>
+            <PreviewActionItem onClick={this.setPreviewMode(previewMode.desktop)}>Desktop</PreviewActionItem>
+          </PreviewActionGroup>
+        </PreviewActions>
+        <FrameContainer >
+          <Frame id='preview-frame' previewMode={mode} src={frameUrl} sandbox='allow-forms allow-scripts allow-same-origin allow-popups' />
+        </FrameContainer>
       </Container>
     )
   }
